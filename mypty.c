e2d56eed6f06b7bd10e3cbca598ef893ee1aa9c1
Jordan Goulder <jordan.goulder@gmail.com>
2017-09-08 23:45:34 -0400

    Print data received over terminal

--- mypty.c
+++ mypty.c
@@ -1,13 +1,20 @@
 #define _XOPEN_SOURCE 600
+#define _DEFAULT_SOURCE
 #include <stdlib.h>
 #include <fcntl.h>
-#include <stdio.h>
 #include <errno.h>
+#include <unistd.h>
+#include <stdio.h>
+#define __USE_BSD
+#include <termios.h>
 
 int main(int argc, char argv[])
 {
     int fdMaster;
+    int fdSlave;
     int error;
+    int count;
+    char input[150];
 
     printf("Before creating pseudo-terminal...\n");
     system("ls -l /dev/pts");
@@ -37,4 +44,22 @@ int main(int argc, char argv[])
     putchar('\n');
 
     printf("The slave side is named: %s\n", ptsname(fdMaster));
+
+    while (1) {
+        count = read(fdMaster, input, sizeof(input) - 1);
+        input[count] = '\0';
+        if (count > 0) {
+            printf("Received %d bytes\n", count);
+            printf("'%s'\n", input);
+            for (int i = 0; i < count; i++) {
+                printf("%02X ", input[i]);
+            }
+            putchar('\n');
+        } else {
+            printf("Read 0 bytes. Exiting.\n");
+            break;
+        }
+    }
+
+    return 0;
 }
