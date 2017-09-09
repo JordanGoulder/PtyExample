0b2f351f087f7c26e051fdcf44c5da3f511b9568
Jordan Goulder <jordan.goulder@gmail.com>
2017-09-09 12:07:21 -0400

    Use select() to read with a timeout

--- mypty.c
+++ mypty.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <linux/limits.h>
+#include <sys/time.h>
+#include <sys/select.h>
 
 #include "hexdump.h"
 #include "pty.h"
@@ -23,15 +25,35 @@ int main(int argc, char argv[])
 
     printf("Listening to pseudo-terminal: %s\n\n", ptyName);
 
+    fd_set rfds;
+    struct timeval tv;
+    int error;
+
     while (1) {
-        count = read(fd, input, sizeof(input) - 1);
-        if (count > 0) {
-            printf("Received %d byte%s\n", count, count == 1 ? ":" : "s:");
-            hexdump(input, count);
-            putchar('\n');
+
+        FD_ZERO(&rfds);
+        FD_SET(fd, &rfds);
+
+        tv.tv_sec = 1;
+        tv.tv_usec = 0;
+
+        error = select(fd + 1, &rfds, NULL, NULL, &tv);
+
+        if (error == -1) {
+            perror("select()");
+        } else if (error) {
+            count = read(fd, input, sizeof(input) - 1);
+            if (count > 0) {
+                printf("\nReceived %d byte%s\n", count, count == 1 ? ":" : "s:");
+                hexdump(input, count);
+                putchar('\n');
+            } else {
+                perror("read()");
+                break;
+            }
         } else {
-            puts("PTY closed.");
-            break;
+            putchar('.');
+            fflush(stdout);
         }
     }
 
